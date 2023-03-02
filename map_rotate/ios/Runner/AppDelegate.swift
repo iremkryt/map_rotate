62090abef475689bde2b1ec7c978066b20588225
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-03-02 16:42:52 +0300

    Added api key and some permissions for Android and iOS

--- map_rotate/ios/Runner/AppDelegate.swift
+++ map_rotate/ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDZfyfTcRvBwFZHYHri2mNSYHtKz90WcHQ")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
